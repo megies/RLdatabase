dd2676246efeb0c05b67b3cebfc9eb3cb3d6a590
Bryant Chow <bryant.h.c@gmail.com>
2017-10-11 15:36:17 +0200

    added stationxmls, crontab and a request script for querying db

---
+++ populate_database/db_request.py
@@ -0,0 +1,134 @@
+"""10.10.17
+A script to interact with the database. Most useful for uploading single stationXML files
+and deleting single events or stationxmls. Wipe is dangerous
+"""
+import os
+import sys
+import glob
+import requests
+import argparse
+
+# command line arguments
+parser = argparse.ArgumentParser(description='Database interaction script for'
+    'adding stations, individual events, attaching individual attachments,'
+    'deleting stations and events or choosing the nuclear option and wiping the'
+    'database clean. All arguments needed except for wipe.',formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--action', help='Performable actions:\n\
+    get: return all XML information\n\
+    put: upload a XML file\n\
+    attach: post an attachment\n\
+    delete: delete an event and or station\n\
+    wipe: wipe all event XML files, no fileid', type=str, default='blank')
+parser.add_argument('--pick',help='Choose [stationxml] or [quakeml]',type=str,
+                                                                default='blank')
+parser.add_argument('--fileid', help='Filename for object as input,\n' 
+    'If [all], will post all stationxml files to database.\n'
+    'For mass event upload, use event_upload_rotjane.py',
+                                                    type=str,default='blank')
+args = parser.parse_args()
+
+action = args.action
+pick = args.pick
+filename = args.fileid
+if (action or pick or filename) == 'blank':
+    sys.exit('Requires all arguments, use -h to see options')
+
+
+# ===============================MAIN==========================================
+# jane settings
+root_path = 'http://127.0.0.1:8000/rest/'
+authority = ('chow','chow')
+
+# folder with stataionXML files, quakeml files
+sta_path = './stationXMLs/'
+eve_path = '/path/to/OUTPUT/'
+
+# actions
+if action == 'get':
+    if filename == 'blank': filename = ''
+    r = requests.get(
+            url=root_path + 'documents/{}/{}'.format(pick,filename),
+            auth=authority)
+
+    assert r.ok
+    print(r.content)
+
+elif action == 'put':
+    if filename == 'all' and pick == 'stationxml':
+        # bit hacky because dataless in a folder, but works
+        stations = glob.glob(sta_path + '*')
+        station_names = [_[len(sta_path):] for _ in stations]
+        for sta,sta_name in zip(stations,station_names):
+            with open(sta,'rb') as fh:
+                r = requests.put(
+                url=root_path + 'documents/{}/{}'.format(pick,sta_name),
+                auth=authority,
+                data=fh)
+
+    else:
+        with open(filename,'rb') as fh:
+            r = requests.put(
+                url=root_path + 'documents/{}/{}'.format(pick,filename),
+                auth=authority,
+                data=fh)
+
+    assert r.ok 
+
+
+elif action == 'delete':
+    r = requests.delete(
+            url=root_path + 'documents/{}/{}'.format(pick,filename),
+            auth=authority)
+
+    assert r.ok
+
+elif action == 'attach':
+    content = input('content-type?: ')    
+    category = input('category?: ')
+    headers = {'content-type': '{}'.format(content),
+                 'category': '{}'.format(category)} 
+
+    doc_ind = input('document index: ')
+    Rtmp = requests.get(
+            url=root_path + 'document_indices/{}/{}'.format(pick,doc_ind),
+            auth=authority)
+    print(Rtmp.content)
+
+    with open(filename,'rb') as fh:
+        r = requests.post(
+            url=root_path + 'document_indices/{}/{}/attachments'.format(
+                                                                pick,doc_ind),
+            auth=authority,
+            headers=headers,
+            data=fh)
+
+    assert r.ok
+
+    assert r.ok
+
+elif action == 'wipe':
+    wipe = input('Are you sure you want to wipe the {} folder?\n'
+                                                    'yes or no?: '.format(pick))
+    if wipe == 'yes' and pick == 'quakeml':
+        filelist = glob.glob(eve_path + 'GCMT*') + glob.glob(eve_path + 'ISC*')
+        filenames = ['{}.xml'.format(_[len(eve_path):]) for _ in filelist]
+        a=1/0
+    elif wipe == 'yes' and pick == 'stationxml':
+        filelist = glob.glob(sta_path + '*')
+        filenames = [_[len(sta_path):] for _ in filelist]   
+        a=1/0 
+    else:
+        sys.exit('Aborting')
+
+    for filename in filelist:
+        try:
+            r = requests.delete(
+                    url=root_path + 'documents/{}/{}'.format(pick,filename),
+                    auth=authority)
+            assert r.ok
+        except AssertionError:
+            continue
+       
+
+
+
--- populate_database/populate_database.sh
+++ populate_database/populate_database.sh
@@ -1,35 +1,45 @@
 # normal script operation, half a year at a time (with exceptions)
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2007-07-18T00:00 --max_datetime 2007-09-13T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2007-09-26T00:00 --max_datetime 2007-12-15T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2007-12-26T23:00 --max_datetime 2008-02-20T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2008-03-16T00:00 --max_datetime 2008-05-06T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2008-06-10T00:00 --max_datetime 2008-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2009-01-01T00:00 --max_datetime 2009-06-30T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2009-07-01T00:00 --max_datetime 2009-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2010-01-01T00:00 --max_datetime 2010-06-30T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2010-07-01T00:00 --max_datetime 2010-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2011-01-01T00:00 --max_datetime 2011-06-30T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2011-07-01T00:00 --max_datetime 2011-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2012-01-01T00:00 --max_datetime 2012-03-14T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2012-05-06T00:00 --max_datetime 2012-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2013-01-01T00:00 --max_datetime 2013-07-09T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2013-09-02T00:00 --max_datetime 2013-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2014-01-01T00:00 --max_datetime 2014-04-30T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2014-11-16T00:00 --max_datetime 2014-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2015-01-01T00:00 --max_datetime 2015-06-30T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2015-07-01T00:00 --max_datetime 2015-12-31T23:59 --mode fdsn
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2016-01-01T00:00 --max_datetime 2016-08-02T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2007-07-18T00:00 --max_datetime 2007-09-13T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2007-09-26T00:00 --max_datetime 2007-12-15T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2007-12-26T23:00 --max_datetime 2008-02-20T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2008-03-16T00:00 --max_datetime 2008-05-06T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2008-06-10T00:00 --max_datetime 2008-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2009-01-01T00:00 --max_datetime 2009-06-30T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2009-07-01T00:00 --max_datetime 2009-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2010-01-01T00:00 --max_datetime 2010-06-30T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2010-07-01T00:00 --max_datetime 2010-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2011-01-01T00:00 --max_datetime 2011-06-30T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2011-07-01T00:00 --max_datetime 2011-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2012-01-01T00:00 --max_datetime 2012-03-14T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2012-05-06T00:00 --max_datetime 2012-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2013-01-01T00:00 --max_datetime 2013-07-09T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2013-09-02T00:00 --max_datetime 2013-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2014-01-01T00:00 --max_datetime 2014-04-30T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2014-11-16T00:00 --max_datetime 2014-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2015-01-01T00:00 --max_datetime 2015-06-30T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2015-07-01T00:00 --max_datetime 2015-12-31T23:59 --mode fdsn
+python ../waveformCompare_20170110_master.py --min_datetime 2016-01-01T00:00 --max_datetime 2016-08-02T23:59 --mode fdsn
 
 # flipped polarities
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2007-09-10T00:00 --max_datetime 2007-09-25T23:59 --mode fdsn --polarity reverse
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2007-12-16T00:00 --max_datetime 2007-12-26T23:00 --mode fdsn --polarity reverse
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2008-02-21T00:00 --max_datetime 2008-03-15T23:59 --mode fdsn --polarity reverse
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2008-05-07T00:00 --max_datetime 2008-06-10T23:59 --mode fdsn --polarity reverse
+# decided not to flip polarities as it will be interesting to filter by negative correlation, leaving calls to show time periods
+# python ../waveformCompare_20170110_master.py --min_datetime 2007-09-10T00:00 --max_datetime 2007-09-25T23:59 --mode fdsn --polarity reverse
+# python ../waveformCompare_20170110_master.py --min_datetime 2007-12-16T00:00 --max_datetime 2007-12-26T23:00 --mode fdsn --polarity reverse
+# python ../waveformCompare_20170110_master.py --min_datetime 2008-02-21T00:00 --max_datetime 2008-03-15T23:59 --mode fdsn --polarity reverse
+# python ../waveformCompare_20170110_master.py --min_datetime 2008-05-07T00:00 --max_datetime 2008-06-10T23:59 --mode fdsn --polarity reverse
 
 # events where STS2 GPS went down (overshoot time periods just in case)
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2014-05-01T00:00 --max_datetime 2014-11-15T23:59 --mode fdsn --instrument lennartz
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2013-07-10T00:00 --max_datetime 2013-09-01T23:59 --mode fdsn --instrument lennartz
-python2.7 ../waveformCompare_20170110_master.py --min_datetime 2012-03-15T00:00 --max_datetime 2012-05-05T23:59 --mode fdsn --instrument lennartz
+python ../waveformCompare_20170110_master.py --min_datetime 2014-05-01T00:00 --max_datetime 2014-11-15T23:59 --mode fdsn --instrument lennartz
+python ../waveformCompare_20170110_master.py --min_datetime 2013-07-10T00:00 --max_datetime 2013-09-01T23:59 --mode fdsn --instrument lennartz
+python ../waveformCompare_20170110_master.py --min_datetime 2012-03-15T00:00 --max_datetime 2012-05-05T23:59 --mode fdsn --instrument lennartz
 
 # extra events from file (ISC catalog tag, not from GCMT catalog)
-python2.7 ../waveformCompare_20170110_master.py --mode qmlfile
+python ../waveformCompare_20170110_master.py --mode qmlfile
+
+# push stationxml files
+python db_request.py --action put --pick stationxml --fileid all
+
+# upload all events from OUTPUT folder
+python ../OUTPUT/event_upload_rotjane.py --timespan all
+
+
+
